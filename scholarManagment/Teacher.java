a3cd8049d342e0a2e20645e1f8b0d1ee046b042b
anponiomss1 <157861282+antoniomss1@users.noreply.github.com>
2025-03-14 09:21:00 -0400

    Corrected package name

---
+++ scholarManagment/Teacher.java
@@ -0,0 +1,22 @@
+package scholar_managment;
+
+import java.util.ArrayList;
+
+public class Teacher{	
+	String nome;
+	ArrayList<Component> components = new ArrayList<>();
+	public void addComponent(Component comp) {
+		this.components.add(comp);	
+	}
+	public void viewStudentsGrades() {
+		for(Component component : components) {
+							// function that list the students grades on the component
+		}
+	}
+	
+	public void viewStudent(Student student, double nota1, double nota2, int cod) {
+
+				// function to add notes to a student	
+	}
+		
+}
